7ca39a83c6c7ac1db8b0c810b41244ce275964f1
d0d061g <davide.blancoinni@gmail.com>
2024-10-16 21:34:04 +0200

    Add files via upload

---
+++ scripts/Peppo Magia e Predizioni.js
@@ -0,0 +1,9 @@
+function prediction() {
+  let randomNumber = Math.random();
+  console.log(randomNumber);
+  if (randomNumber < 0.5) {
+    alert("Hai Cacato nel Puzzo");
+  } else {
+    alert("Non Hai Cacato nel Puzzo");
+  }
+};
