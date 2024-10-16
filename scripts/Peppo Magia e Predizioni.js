9d75e1e01b5a529cfc96e8abdcb85aed022ddc2d
d0d061g <davide.blancoinni@gmail.com>
2024-10-16 21:44:07 +0200

    Update Peppo Magia e Predizioni.js

--- scripts/Peppo Magia e Predizioni.js
+++ scripts/Peppo Magia e Predizioni.js
@@ -1,5 +1,6 @@
+let randomNumber = Math.random();
+
 function prediction() {
-  let randomNumber = Math.random();
   console.log(randomNumber);
   if (randomNumber < 0.5) {
     alert("Hai Cacato nel Puzzo");
